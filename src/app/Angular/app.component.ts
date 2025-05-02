d5fb6bffb2579e08b7468e5e51686e1670387a65
PUTrades005 <jaxonholdenn40@gmail.com>
2025-05-02 16:51:31 -0400

    Make AppComponent standalone pull in RouterModule; import AppComponent & DashboardComponent in AppModule

--- src/app/Angular/app.component.ts
+++ src/app/Angular/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    RouterModule    // ← add this line
+  ]
 })
-export class AppComponent {
-  title = 'dashboard-app';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './Angular/app.component';
 import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './Angular/app.component';       // standalone
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes),
-    DashboardComponent // <-- Import standalone component here
+    RouterModule.forRoot(routes),  // sets up <router-outlet>
+    AppComponent,                  // standalone root
+    DashboardComponent             // standalone dashboard
   ],
   providers: [],
   bootstrap: [AppComponent]
